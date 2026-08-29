cb87e9dbe18dbe683f220dd90c1f760d57e7b33e
Miaoyww <miaomiaoywww@gmail.com>
2026-08-29 18:09:03 +0800

    feat: 从MainWindow中迁移SettingsWindow

--- src/AFMediaBar/ViewModels/Windows/MainWindowViewModel.cs
+++ src/AFMediaBar/ViewModels/Windows/MainWindowViewModel.cs
@@ -5,53 +5,6 @@ namespace AFMediaBar.ViewModels.Windows
 {
     public partial class MainWindowViewModel : ObservableObject
     {
-        [ObservableProperty]
-        private string _applicationTitle = "WPF UI - AFMediaBar";
-
-        [ObservableProperty]
-        private ObservableCollection<object> _menuItems = new()
-        {
-            new NavigationViewItem()
-            {
-                Content = "General",
-                Icon = new SymbolIcon { Symbol = SymbolRegular.Home24 },
-                TargetPageType = typeof(Views.Pages.GeneralPage)
-            },
-            new NavigationViewItem()
-            {
-                Content = "Appearance",
-                Icon = new SymbolIcon { Symbol = SymbolRegular.Color16 },
-                TargetPageType = typeof(Views.Pages.AppearancePage)
-            },
-            new NavigationViewItem()
-            {
-                Content = "Layout",
-                Icon = new SymbolIcon { Symbol = SymbolRegular.DataHistogram24 },
-                TargetPageType = typeof(Views.Pages.LayoutPage)
-            }
-        };
-
-        [ObservableProperty]
-        private ObservableCollection<object> _footerMenuItems = new()
-        {
-            new NavigationViewItem()
-            {
-                Content = "Settings",
-                Icon = new SymbolIcon { Symbol = SymbolRegular.Settings24 },
-                TargetPageType = typeof(Views.Pages.SettingsPage)
-            },
-            new NavigationViewItem()
-            {
-                Content = "About",
-                Icon = new SymbolIcon { Symbol = SymbolRegular.Info24 },
-                TargetPageType = typeof(Views.Pages.AboutPage)
-            }
-        };
-
-        [ObservableProperty]
-        private ObservableCollection<MenuItem> _trayMenuItems = new()
-        {
-            new MenuItem { Header = "Home", Tag = "tray_home" }
-        };
+        [ObservableProperty] private string _applicationTitle = "AFMediaBar";
     }
 }
